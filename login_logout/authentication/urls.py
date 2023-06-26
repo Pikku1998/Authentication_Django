6b34f0ea8fe97d8891742870caad515d84b2b85a
Prakash R <prakashramanukolanu1998@gmail.com>
2023-06-26 21:50:22 +0530

    Add sign_out function based view.

--- login_logout/authentication/urls.py
+++ login_logout/authentication/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from . views import sign_in, register
+from . views import register, sign_in, sign_out
 
 urlpatterns = [
-    path('', sign_in, name='sign_in'),
     path('register', register, name='register'),
-
+    path('', sign_in, name='sign_in'),
+    path('signout', sign_out, name='sign_out'),
 ]
--- login_logout/authentication/views.py
+++ login_logout/authentication/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
-from django.contrib.auth import authenticate
+from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.http import HttpResponse
 
@@ -25,23 +25,31 @@ def register(request):
         else:
             User.objects.create_user(username=username, password=password1, email=email)
             messages.info(request, "Login with your credentials")
-            return redirect('')
+            return redirect('sign_in')
     else:
         return render(request, register_page)
 
 
 def sign_in(request):
+    sign_in_page = 'login_page.html'
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
         user = authenticate(username=username, password=password)
         if user:
+            login(request, user)
             return render(request, 'homepage.html', context={'username': username})
         else:
             messages.error(request, "No user exists")
-            return render(request, 'login_page.html')
+            return render(request, sign_in_page)
 
-    return render(request, 'login_page.html')
+    return render(request, sign_in_page)
+
+
+def sign_out(request):
+    logout(request)
+    return redirect('sign_in')
+
 
 
 
